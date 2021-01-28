e87ce85e57102998df54d987d3262d8052cd9ee0
kantaro.sasajima <kan.0922.baseball@softbank.ne.jp>
2021-01-28 22:23:57 +0900

    seedファイル仮

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..3).map do |i|
+  User.create!(
+    email: "user#{i}@example.com",
+    password: 'password',
+    password_confirmation: 'password'
+  )
+end
+
+# 一人目のユーザーだけに子モデル追加
+user = User.first
+user.categories.create!(
+  (1..rand(4..6)).map do |i|
+    {
+      name: "カテゴリー名_#{i}",
+      todos_attributes:
+        (1..rand(2..3)).map do |j|
+          {
+            title: "todo_#{i}_#{j}",
+            content: "内容_#{i}_#{j}"
+          }
+        end
+    }
+  end
+)
