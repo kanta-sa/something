4a8250bc7bbf0b49e4e61be35fa4a12fbcef3c8a
kantaro.sasajima <kan.0922.baseball@softbank.ne.jp>
2021-02-01 11:13:05 +0900

    モデルに追加

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,8 @@
 class Todo < ApplicationRecord
+  # モジュール
+  extend Enumerize
+  enumerize :status, in: [:yet, :done], default: :yet
+
   # 関連
   belongs_to :category
 
@@ -7,4 +11,5 @@ class Todo < ApplicationRecord
                         length: { maximum: 20 }
   validates :content,   presence: true,
                         length: { maximum: 255 }
+  validates :status,    presence: true
 end
