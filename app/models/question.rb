3449480bc38e6aeb87fbbf6b15fe4072129b76f9
kantaro.sasajima <kan.0922.baseball@softbank.ne.jp>
2021-02-11 11:36:46 +0900

    fix

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,10 +3,12 @@ class Question < ApplicationRecord
   belongs_to :user
   has_many :user_questions
   has_many :users, through: :user_questions
+  has_many :choices
+  accepts_nested_attributes_for :choices, allow_destroy: true, reject_if: :all_blank
 
   # バリデーション
   validates :title,         presence: true,
                             length: { maximum: 30 }
-  validates :answered,      presence: true
+  # validates :answered,    presence: true
   validates :user,          presence: true
 end
