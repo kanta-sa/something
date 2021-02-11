bf0729a4bde5ae95b89092dbffcc4b5a21f27356
kantaro.sasajima <kan.0922.baseball@softbank.ne.jp>
2021-02-11 11:37:40 +0900

    choiceフィールド作成

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,8 +5,23 @@ class QuestionsController < ApplicationController
 
   def new
     @question = current_user.questions.new
+    @question.choices.build
   end
 
   def create
+    @question = current_user.questions.build(question_params)
+    binding.pry
+    if @question.save
+      flash[:notice] = 'アンケートを作成しました。'
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, choices_attributes: [:content, :_destroy])
   end
 end
