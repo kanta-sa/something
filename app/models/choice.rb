19b5e1559680cdfdc3c91ff58d2b9639a98653af
kantaro.sasajima <kan.0922.baseball@softbank.ne.jp>
2021-02-13 13:51:08 +0900

    アンケート結果可視化

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -6,4 +6,17 @@ class Choice < ApplicationRecord
   validates :content,         presence: true,
                               length: { maximum: 20 }
   validates :question,        presence: true
+
+  # メソッド
+  def choice_score_percentage
+    total = 0
+    question.choices.each do |choice|
+      total += choice.ans_cnt
+    end
+    if ans_cnt
+      ((ans_cnt.to_f/total)*100).round(1)
+    else
+      0.0
+    end
+  end
 end
